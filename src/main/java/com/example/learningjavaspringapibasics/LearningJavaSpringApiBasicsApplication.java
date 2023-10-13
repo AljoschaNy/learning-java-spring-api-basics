743e6e343e7525f5d7a6a2bc31b9aefd42c6f670
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-13 10:50:58 +0200

    Initial commit

---
+++ src/main/java/com/example/learningjavaspringapibasics/LearningJavaSpringApiBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningjavaspringapibasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJavaSpringApiBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningJavaSpringApiBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningjavaspringapibasics/LearningJavaSpringApiBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningjavaspringapibasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJavaSpringApiBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
