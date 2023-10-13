b957be7da7e291211f214159ea311091184c46e9
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-13 11:49:01 +0200

    Add addMessage and getMessages methods

--- src/main/java/com/example/learningjavaspringapibasics/GreetingController.java
+++ src/main/java/com/example/learningjavaspringapibasics/GreetingController.java
@@ -2,8 +2,15 @@ package com.example.learningjavaspringapibasics;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class GreetingController {
+    private ArrayList<Message> messages = new ArrayList<>();
+
+
     @GetMapping("/hello")
     public String greetWorld() {
         return "Hello, World!";
@@ -12,5 +19,16 @@ public class GreetingController {
     @GetMapping("hello/{name}")
     public String greetWorld(@PathVariable String name) {
         return "Hello, " + name +"!";
+    }
+
+    @PostMapping("/messages")
+    public ArrayList<Message> addMessage(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        messages.add(new Message(id,name,message));
+        return messages;
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessages() {
+        return messages;
     }
 }
